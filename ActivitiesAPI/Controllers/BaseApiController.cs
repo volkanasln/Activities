f2f24c9650c8c5ea8299ff67311f5d6fbcd44167
Volkan Aslan <volkanaslan@live.com>
2022-01-15 19:40:33 +0300

    Activities first checkin

---
+++ ActivitiesAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActivitiesAPI.Controllers
+{
+    [ApiController]
+    [Route("activitiesapi/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
