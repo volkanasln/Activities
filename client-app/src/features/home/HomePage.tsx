651ecdb6923aa75847132ecc64bbfd26a98e31c7
Volkan Aslan <volkanaslan@live.com>
2022-02-13 17:45:09 +0300

    Styling

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Container } from "semantic-ui-react";
+import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
 
 export default function HomePage(){
     return(
-        <Container style={{marginTop: '7em'}}>
-            <h1> Home Page </h1>
-            <h3> Go To <Link to='activities'> Activities </Link> </h3>
-        </Container> 
+        <Segment inverted textAlign="center" vertical className="masthead">
+            <Container text>
+                <Header as="h1" inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom :12}} />
+                    Activities
+                </Header>
+                <Header as='h2' inverted content='Welcome to Activities' />
+                <Button as={Link} to='/activities' size='huge' inverted>
+                Take me to Activities! 
+                </Button> 
+            </Container>
+
+        </Segment>
     ) 
 }
